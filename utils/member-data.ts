7f67189e9e7e9329ad98de907caf830a17499783
thebeyondr <jeopatto@gmail.com>
2022-06-08 20:14:51 -0500

    feat: :sparkles: Add project & member data

---
+++ utils/member-data.ts
@@ -0,0 +1,66 @@
+import { Member } from '../interfaces';
+
+export const MemberData: Member[] = [
+    {
+        name: 'sinu',
+        description: 'TLSNotary Project Lead and Software Engineer',
+        links: [
+            {
+                github: 'https://github.com/sinui0',
+            },
+            {
+                twitter: 'https://twitter.com/sinu_eth',
+            },
+        ],
+    },
+    {
+        name: 'AtHeartEngineer',
+        description: 'Full stack / systems engineer; working on RLN',
+        links: [
+            {
+                github: 'https://github.com/atheartengineer',
+            },
+            {
+                twitter: 'https://twitter.com/atheartengineer',
+            },
+        ],
+    },
+    {
+        name: 'Geoff Lamperd',
+        description:
+            'Full stack engineer; working on Trusted Setup and Interep',
+        links: [
+            {
+                github: 'https://github.com/glamperd',
+            },
+            {
+                twitter: 'https://twitter.com/glamperd',
+            },
+        ],
+    },
+    {
+        name: 'James Zaki',
+        description: 'BLS Wallet project lead and software engineer',
+        links: [
+            {
+                github: 'https://github.com/jzaki',
+            },
+            {
+                twitter: 'https://twitter.com/james_zaki',
+            },
+        ],
+    },
+    {
+        name: 'Beyondr (Jeo)',
+        description:
+            'Designer & developer. Main project is ZKOPRU at the moment but also working on other auxillary projects.',
+        links: [
+            {
+                github: 'https://github.com/thebeyondr',
+            },
+            {
+                twitter: 'https://twitter.com/thebeyondr',
+            },
+        ],
+    },
+];
