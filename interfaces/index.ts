afe436784bda5cc591526f1a01fee8594bb355d6
thebeyondr <jeopatto@gmail.com>
2022-06-08 20:13:49 -0500

    feat: :sparkles: Add member & project interfaces

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,10 +1,18 @@
-// You can include shared interfaces/types in a separate file
-// and then use them in any component by importing them. For
-// example, to import the interface below do:
-//
-// import { User } from 'path/to/interfaces';
+export type Member = {
+    name: string;
+    description: string;
+    links: [{ github: string }?, { twitter?: string }?];
+};
 
-export type User = {
-  id: number
-  name: string
-}
+export type Project = {
+    name: string;
+    shortName?: string;
+    image: string;
+    description: string;
+    links: {
+        website?: string;
+        github?: string;
+        telegram?: string;
+        discord?: string;
+    };
+};
